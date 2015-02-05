026e85dc71d2605ceb490bf412747efa0a8286bd
Kevin Goess <knet@goess.org>
2015-02-04 17:21:38 -0800

    all four working, with nice print

--- src/github.com/kgoess/start-services/start-services.go
+++ src/github.com/kgoess/start-services/start-services.go
@@ -6,71 +6,56 @@ import (
         "gopkg.in/yaml.v2"
 )
 
-var data = `
+var yamlStr = `
 ---
-config: 
-#task1:
-#  cmd:
-#    - /bin/echo
-#    - task 1 is running
-#  descr: this task just runs echo
-#task2:
-#  cmd:
-#    - /bin/sleep
-#    - 3
-#  descr: sleeps for a couple secs
-#task3:
-#  cmd:
-#    - /bin/ls
-#    - adsfadfasdf
-#  descr: this is expected to fail
-     - task4:
-          after:
-            - task1
-            - task2
-          cmd:
-            - /bin/echo
-            - 99
-         descr: this task goes after 1 and 2
-`
-var data2 = `
----
-items: 
-     - name: task3
-       after: 
-       cmd:
-         - /bin/ls
-         - adsfadfasdf
-       descr: this is expected to fail
-     - name: task4
-       after:
-          - task1
-          - task2
-       cmd:
-         - /bin/echo
-         - 99
-       descr: this task goes after 1 and 2
+task1:
+  cmd:
+    - /bin/echo
+    - task 1 is running
+  descr: this task just runs echo
+task2:
+  cmd:
+    - /bin/sleep
+    - 3
+  descr: sleeps for a couple secs
+task3:
+  after: 
+  cmd:
+    - /bin/ls
+    - adsfadfasdf
+  descr: this is expected to fail
+task4:
+  after:
+     - task1
+     - task2
+  cmd:
+    - /bin/echo
+    - 99
+  descr: this task goes after 1 and 2
 `
 type TaskConfig struct {
-    Name string
     After []string
     Cmd []string
     Descr string
 }
-type Configs struct {
-    Items []TaskConfig 
-}
+//type Configs struct {
+//    Items []TaskConfig 
+//}
 
 func main(){
-    //configs := map[string]taskConfig{}
-    configs := Configs{}
+    configs := map[string]TaskConfig{}
+    //configs := Configs{}
 
-    err := yaml.Unmarshal([]byte(data2), &configs)
+    err := yaml.Unmarshal([]byte(yamlStr), &configs)
     if err != nil {
         log.Fatalf("error: %v", err)
     }
 
-    fmt.Printf("--- configs are:\n%v\n\n", configs)
+    for key, task := range configs {
+        fmt.Printf("task:  %v (run after: %v)\ndescr: %v\n\t%v\n------------------\n",
+             key, task.After, task.Descr, task.Cmd)
+    }
+
 }
 
 /*
